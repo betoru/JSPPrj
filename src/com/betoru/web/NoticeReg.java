cc725773a6e318ac5162e2da99eab0afa41c31e7
betoru <betoru@naver.com>
2022-03-08 14:51:30 +0900

    desk 작업

---
+++ src/com/betoru/web/NoticeReg.java
@@ -0,0 +1,27 @@
+package com.betoru.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/notice-reg")
+public class NoticeReg extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setCharacterEncoding("UTF-8");
+//        resp.setContentType("text/html; charSet=UTF-8");
+        req.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+
+        out.println(title);
+        out.println(content);
+    }
+}
